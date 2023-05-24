940a7677fdd1666d0b875639b2a6687b1d28637c
silviosalza <silviofilipposalza@gmail.com>
2023-05-24 17:17:48 +0200

    movies db captured

--- app/Http/Controllers/HomeController.php
+++ app/Http/Controllers/HomeController.php
@@ -9,7 +9,7 @@ class HomeController extends Controller
 {
     public function index(){
         $movies = Movie::all();
-        // dd($movies);
+        //  dd($movies);
         return view('home', compact('movies'));
     }
 }
--- resources/views/home.blade.php
+++ resources/views/home.blade.php
@@ -2,6 +2,22 @@
 
 @section('content')
 
-<h1>ciao</h1>
+<div class="comic-container py-4">
+    <div class="container">
+        <div class="row row-cols-6 g-3">
+            @foreach ($movies as $movie)
+                <div class="col">
+                    <div class="card">
+                        <div class="card-body">
+                            <img src="{{$movie -> image}}" alt="">
+                            <h3 class="card-title">{{$movie -> title}}</h3>
+                        </div>
+                    </div>
+                </div>
+            @endforeach
+        </div>
+    </div>
+</div>
+
 
 @endsection

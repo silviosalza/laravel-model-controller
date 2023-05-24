572b6733e074fcb4f28047609d13d65c31614187
silviosalza <silviofilipposalza@gmail.com>
2023-05-24 17:02:54 +0200

    first commit

---
+++ app/Http/Controllers/HomeController.php
@@ -0,0 +1,15 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use App\Models\Movie;
+use Illuminate\Http\Request;
+
+class HomeController extends Controller
+{
+    public function index(){
+        $movies = Movie::all();
+        // dd($movies);
+        return view('home', compact('movies'));
+    }
+}
